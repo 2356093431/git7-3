41d19a650b6813665241ffdf0656384645b77b39
2356093431 <2356093431@qq.com>
2019-07-03 17:09:24 +0800

    “新增实体类

---
+++ src/main/java/com/jk/model/Order.java
@@ -0,0 +1,34 @@
+package com.jk.model;
+
+public class Order {
+
+    private Integer Id;
+
+    private String name;
+
+    private String  time;
+
+    public Integer getId() {
+        return Id;
+    }
+
+    public void setId(Integer id) {
+        Id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+}
